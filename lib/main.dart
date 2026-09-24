d903d55d6137bb7c6df005dd04c3661b0c0ba0d8
sdkd2039 <94754167+sdkd2039@users.noreply.github.com>
2026-09-24 15:41:38 +0300

    Create lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'معدودات',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const MainWebViewScreen(),
+    );
+  }
+}
+
+class MainWebViewScreen extends StatefulWidget {
+  const MainWebViewScreen({super.key});
+
+  @override
+  State<MainWebViewScreen> createState() => _MainWebViewScreenState();
+}
+
+class _MainWebViewScreenState extends State<MainWebViewScreen> {
+  late final WebViewController _controller;
+  bool _isLoading = true;
+
+  // رابط موقعك الحالي
+  final String _websiteUrl = 'https://sdkd2039.github.io/Numbered/';
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (String url) {
+            setState(() {
+              _isLoading = true;
+            });
+          },
+          onPageFinished: (String url) {
+            setState(() {
+              _isLoading = false;
+            });
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(_websiteUrl));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            WebViewWidget(controller: _controller),
+            if (_isLoading)
+              const Center(
+                child: CircularProgressIndicator(),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
